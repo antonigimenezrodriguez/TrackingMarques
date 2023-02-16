c1f6f91043c2911abc0a4579cce310449699fa0a
Antoni Gimenez <antoni.gimenez@outlook.es>
2023-02-16 16:45:10 +0100

    Canviem l'estructura de l'xml

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,7 +5,7 @@ namespace TrackingMarques;
 public partial class MainPage : ContentPage
 {
     XmlDocument doc { get; set; }
-    XmlElement gpx;
+    XmlElement punts;
     private CancellationTokenSource _cancelTokenSource;
 
     public MainPage()
@@ -20,17 +20,10 @@ public partial class MainPage : ContentPage
         XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
         XmlElement root = doc.DocumentElement;
         doc.InsertBefore(xmlDeclaration, root);
-        gpx = doc.CreateElement(string.Empty, "gpx", string.Empty);
-        gpx.SetAttribute("version", "1.1");
-        gpx.SetAttribute("creator", "Institut Montilivi");
-        gpx.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-        gpx.SetAttribute("xmlns", "http://www.topografix.com/GPX/1/1");
-        gpx.SetAttribute("xsi:schemaLocation", "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd");
-        gpx.SetAttribute("xmlns:gpxtpx", "http://www.garmin.com/xmlschemas/TrackPointExtension/v1");
-        doc.AppendChild(gpx);
+        punts = doc.CreateElement(string.Empty, "punts", string.Empty);
+        doc.AppendChild(punts);
         await anyadirWPT("Punt Inici de la ruta");
         PuntInteresBtn.IsEnabled = true;
-
     }
 
     private async void PuntInteresBtn_Clicked(object sender, EventArgs e)
@@ -77,38 +70,36 @@ public partial class MainPage : ContentPage
 
         Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
 
-        XmlElement wpt = doc.CreateElement(string.Empty, "wpt", string.Empty);
-        wpt.SetAttribute("lat", location.Latitude.ToString());
-        wpt.SetAttribute("lon", location.Longitude.ToString());
+        XmlElement punt = doc.CreateElement(string.Empty, "punt", string.Empty);
 
-
-        XmlElement time = doc.CreateElement(string.Empty, "time", string.Empty);
+        XmlElement dataHora = doc.CreateElement(string.Empty, "dataHora", string.Empty);
         string fechaXML = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
         XmlText textFecha = doc.CreateTextNode(fechaXML);
 
-        XmlElement ele = doc.CreateElement(string.Empty, "ele", string.Empty);
-        XmlText textEle = doc.CreateTextNode("3.4");
-        ele.AppendChild(textEle);
+        XmlElement elevacio = doc.CreateElement(string.Empty, "elevacio", string.Empty);
+        XmlText textElevacio = doc.CreateTextNode(location.Altitude?.ToString());
+        elevacio.AppendChild(textElevacio);
+
+        XmlElement latitut = doc.CreateElement(string.Empty, "latitut", string.Empty);
+        XmlText textLatitut = doc.CreateTextNode(location.Latitude.ToString());
+        latitut.AppendChild(textLatitut);
+
+        XmlElement longitut = doc.CreateElement(string.Empty, "longitut", string.Empty);
+        XmlText textLongitut = doc.CreateTextNode(location.Longitude.ToString());
+        longitut.AppendChild(textLongitut);
 
         XmlElement nom = doc.CreateElement(string.Empty, "nom", string.Empty);
         XmlText textNom = doc.CreateTextNode(nombre);
+       
+        
         nom.AppendChild(textNom);
-
-        /*XmlElement extensions = doc.CreateElement(string.Empty, "extensions", string.Empty);
-        XmlElement trackPointExtension = doc.CreateElement(string.Empty, "gpxtpx:TrackPointExtension", string.Empty);
-        XmlElement hr = doc.CreateElement(string.Empty, "gpxtpx:hr", string.Empty);
-        XmlText textHr = doc.CreateTextNode("171");
-        hr.AppendChild(textHr);
-        trackPointExtension.AppendChild(hr);
-        extensions.AppendChild(trackPointExtension);
-
-
-        wpt.AppendChild(extensions);*/
-        wpt.AppendChild(nom);
-        wpt.AppendChild(ele);
-        time.AppendChild(textFecha);
-        wpt.AppendChild(time);
-        gpx.AppendChild(wpt);
+        punt.AppendChild(nom);
+        punt.AppendChild(latitut);
+        punt.AppendChild(longitut);
+        punt.AppendChild(elevacio);
+        dataHora.AppendChild(textFecha);
+        punt.AppendChild(dataHora);
+        punts.AppendChild(punt);
     }
 }
 
